e60c354ffa4cd4c06e517ac4f91f96f4a238c20b
sofiastroustrup <sstroustrup@gmail.com>
2025-08-18 15:48:05 +0200

    ancestral reconstruction using phytools

--- experiments/comparison/fastAnc.R
+++ experiments/comparison/fastAnc.R
@@ -2,17 +2,50 @@
 library(tidyverse)
 library(geomorph)
 library(ape)
-setwd("/Users/lkn315/Desktop")
+library(here)
+library(phytools)
 
 # load simulated data set 
-simdata <- read.csv("flat_true_tree.csv", header=FALSE)
-leaf_idx <- c(3, 4, 5, 7, 8)+1 # we add one because R is 1 indexed 
-leaves <- slice(simdata, leaf_idx)
-
-tree <- read.tree("../data/chazot_subtree.nw")
-
-# procrustes align simulated data 
-gpa_simdata <- gpagen(leaves, print.progress = T,Proj=F) # # GPA-alignment
+simdata <- read_csv(here("SPMS/experiments/comparison/simdata/procrustes_aligned_rotated.csv"), col_names=FALSE)%>% t()
+tree <- read.tree(here("SPMS/experiments/data/chazot_subtree.nw"))
+#tree <- read.newick(here("SPMS/experiments/data/chazot_subtree.nw")) 
+tree$tip.label <- c("A", "B", "C", "D", "E") # set names of tips to match simulated data
+colnames(simdata) <- tree$tip.label
+print(simdata)
 
 # do ancestral reconstruction
-anc_recon <- fastAnc(tree, gpa_simdata$coords, model="BM")
+# fastAnc uses felsensteins pruning algorithm and reroots the tree to get MLE
+# sigma^2 is not needed to estimate the mean
+anc_recon <- fastAnc(tree, simdata[1,], model="BM")
+
+
+n_traits <- nrow(simdata)
+n_tips <- length(tree$tip.label)
+n_nodes <- tree$Nnode
+
+# Create empty matrix to store results
+# Columns: internal nodes, Rows: traits
+anc_matrix <- matrix(NA, nrow=n_traits, ncol=n_nodes)
+colnames(anc_matrix) <- (n_tips + 1):(n_tips + n_nodes)  # Node IDs
+rownames(anc_matrix) <- paste0("trait", 1:n_traits)      # Trait names
+
+# Loop through all traits in simdata
+for (i in 1:n_traits) {
+  # Extract trait vector and ensure it has names
+  trait_vector <- simdata[i,]
+  names(trait_vector) <- tree$tip.label
+  
+  # Perform ancestral state reconstruction
+  anc_result <- fastAnc(tree, trait_vector, model="BM")
+  
+  # Store the results in the matrix
+  anc_matrix[i,] <- anc_result
+}
+write.csv(anc_matrix, file=here("SPMS/experiments/comparison/simdata/anc_recon.csv"))
+
+# plot different ancestral states 
+plot(anc_matrix[,2][seq(1, 40, 2)], anc_matrix[,1][seq(2, 40, 2)])
+
+# plot tree to see which nodes numbers refer to 
+plot(tree)
+nodelabels()  # 
--- experiments/comparison/procrustes.R
+++ experiments/comparison/procrustes.R
@@ -1,11 +1,70 @@
 library(geomorph)
+library(tidyverse)
 library(here)
+library(stringr)
 
-
-sim <- read.table(here("experiments/comparison/simdata/leaves.csv"), header=TRUE)
+sim <- read_csv(here("SPMS/experiments/comparison/simdata/leaves.csv"), col_names=TRUE)
 print(sim)
 
-# reshape to match geomorph: n_landmarks x dim x species 
+# reshape simulated data to fit with geomorph 
+sim_mat <- as.matrix(sim[,-1])
+reshaped_sim <- sapply(lapply(1:5, function(ii) {
+  matrix(sim[ii,-1], byrow = TRUE, nrow = 20, ncol = 2)
+}), identity, simplify = "array")
+print(reshaped_sim)
 
+# test 
+test <- array(unlist(reshaped_sim), dim=c(20,2,5))
 
 # Procrustes align data and export 
+proc <- gpagen(test, Proj=FALSE)
+
+proc_rotated <- fixed.angle(proc, angle=45)#rotate.coords(proc, type="flipY")
+plot(proc_rotated)
+
+
+# Rotate landmarks by 45 degrees
+rotate_landmarks <- function(coords, angle_degrees) {
+  # Convert degrees to radians
+  angle_rad <- angle_degrees * pi / 180
+  
+  # Create rotation matrix
+  rot_matrix <- matrix(c(cos(angle_rad), -sin(angle_rad), 
+                         sin(angle_rad), cos(angle_rad)), 
+                       nrow=2, byrow=TRUE)
+  
+  # Get number of specimens
+  n_specimens <- dim(coords$coords)[3]
+  
+  # Create array to store rotated coordinates
+  rotated_coords <- coords
+  
+  # Apply rotation to each specimen
+  for(i in 1:n_specimens) {
+    # Extract landmarks for specimen i
+    specimen_coords <- coords$coords[,,i]
+    
+    # Rotate landmarks using matrix multiplication
+    rotated_coords$coords[,,i] <- specimen_coords %*% rot_matrix
+  }
+  
+  return(rotated_coords)
+}
+
+# Apply 45 degree rotation
+proc_45deg <- rotate_landmarks(proc, 45)
+
+# Plot original and rotated landmarks
+par(mfrow=c(1,2))
+plot(proc)
+plot(proc_45deg)
+
+# export procrustes aligned and rotated data 
+proc_final <- array_reshape(aperm(proc_45deg$coords, c(3, 1, 2)), c(5, 40))
+write.table(proc_final, file=here("SPMS/experiments/comparison/simdata/procrustes_aligned_rotated.csv"), row.names=FALSE, col.names=FALSE, sep=",")
+
+
+# use geomorph for ancestral state reconstruction 
+
+# load phylogenetic tree
+# set names of dimenions
