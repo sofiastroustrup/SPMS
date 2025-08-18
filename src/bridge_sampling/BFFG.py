18d76fc8025cf720a7566cb1c711a3ca81945735
sofiastroustrup <sstroustrup@gmail.com>
2025-08-18 17:42:37 +0200

    replaced jnp.linalg.inv with solve

--- src/bridge_sampling/BFFG.py
+++ src/bridge_sampling/BFFG.py
@@ -34,7 +34,7 @@ def backward_filter(node, theta, sigma):
         H_T = jnp.sum(jnp.array([child.message['H'][0] for child in children]),0)
         #c_T = jnp.sum(jnp.array([child.message['c'](0.,theta) for child in children]),0) 
         Mdagger = C =  jnp.linalg.inv(H_T) # use solve instead?
-        v = jnp.dot(Mdagger,F_T) 
+        v = jnp.linalg.solve(H_T, F_T)#jnp.dot(Mdagger,F_T) 
         subtree_var = 1./jnp.sum(jnp.array([1./(child.T+child.message['subtree_var']) for child in children]),0)
         tildea = Mdagger/subtree_var
         
